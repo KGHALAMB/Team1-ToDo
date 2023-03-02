9203a7d3f6f3635a68a654d9a3611178ee28d38a
kavehghalambor <kghalamb@calpoly.edu>
2023-03-02 14:54:48 -0800

    removing extra code

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -65,28 +65,6 @@ function MyApp() {
     });
   }, []);
 
-  function removeOneCharacter(index) {
-    deleteOne(index).then((result) => {
-      if (result && result.status === 204) {
-        const updated = characters.filter((character, i) => {
-          return i !== index;
-        });
-        setCharacters(updated);
-      } else {
-        console.log('error');
-      }
-    });
-  }
-  function strikeThrough(task) {
-    strike(task.title);
-  }
-  function updateList(person) {
-    makePostCall(person).then((result) => {
-      if (result && result.status === 201) {
-        setCharacters([...characters, result.data]);
-      }
-    });
-  }
   return (
     // This is what we had before:
     /**<div className="container">
